5827c22e6c2aafdca9c13435b2d49058b5550f9d
Kashish Jain <jainabhishek7204@gmail.com>
2021-01-20 17:26:17 +0530

    Responsive UI fixes
    Receipt image not shown directly.

--- src/Components/HomeStats.js
+++ src/Components/HomeStats.js
@@ -7,22 +7,28 @@ function HomeStats({ sent, received }) {
     <div className="numbers">
       <div className="number__item">
         <Statistic
-          title="Received"
+          title="You Got"
           value={received}
           valueStyle={{ color: "#3f8600", textAlign: "center" }}
         />
       </div>
       <div className="number__item">
         <Statistic
-          title="Sent"
+          title="You Gave"
           value={sent}
           valueStyle={{ color: "#cf1322", textAlign: "center" }}
         />
       </div>
       <div className="number__item">
         <Statistic
-          title="Balance"
-          value={received - sent}
+          title={
+            received === sent
+              ? "Balance"
+              : received >= sent
+              ? "You Will Give"
+              : "You Will Get"
+          }
+          value={Math.abs(received - sent)}
           valueStyle={{
             color: received >= sent ? "#3f8600" : "#cf1322",
             textAlign: "center",
